cf32545f635d9eb4ad919e840d986a7e8f3468de
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-02 00:45:57 +0530

    css and logic modified
    replaced the .keyCode to .key and included the transition of css

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
+    const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
     if(!audio) return;
+    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
     // console.log(e.key);
