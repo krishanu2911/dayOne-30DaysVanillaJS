8ef1d2aa830c845dd6439f5d48d6fbbdf426a214
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-02 01:40:18 +0530

    update: playsound function

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', playSound);
+//  function(e){
+//     const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
+//     const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
+//     if(!audio) return;
+//     key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+//     // console.log(e.key);
+//     // console.log(audio);
+// });
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
     const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
     if(!audio) return;
@@ -8,10 +19,22 @@ window.addEventListener('keydown', function(e){
     audio.play();
     // console.log(e.key);
     // console.log(audio);
-});
+}
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
+    const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
+    if(!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+    // console.log(e.key);
+    // console.log(audio);
+}
 function removeTransition(event){
     if(event.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
+
 const keys = document.querySelectorAll('.key');
+
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
