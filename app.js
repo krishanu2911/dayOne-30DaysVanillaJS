7bdca9a13824788c0e89d0d48b164595cfb90a4a
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-02 06:55:41 +0530

    cleaned code

--- app.js
+++ app.js
@@ -20,16 +20,7 @@ function playSound(e){
     // console.log(e.key);
     // console.log(audio);
 }
-function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
-    const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
-    if(!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-    // console.log(e.key);
-    // console.log(audio);
-}
+
 function removeTransition(event){
     if(event.propertyName !== 'transform') return;
     this.classList.remove('playing');
